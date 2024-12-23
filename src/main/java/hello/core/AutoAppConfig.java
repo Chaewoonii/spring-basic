8575185f6f37633b3d19b97b60f7e200cad38b39
chaeun kim <cocodata12@gmail.com>
2024-12-23 15:24:47 +0900

    의존관계 주입 방법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.FilterType;
 // 컴포넌트 스캔 만으로는 의존관계 주입이 불가능하므로 @Autowired 라는 애노테이션을 사용(자동 의존관계 주입): ~ServiceImpl에서 사용
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", //basePackage: 탐색 위치 지정. member 패키지만 스캔.
+//        basePackages = "hello.core", //basePackage: 탐색 위치 지정. member 패키지만 스캔.
 //        basePackageClasses : 지정한 클래스의 패키지를 탐색 시작 위치로 지정. 지정하지 않으면 @Configuration 이 붙은 클래스의 패키지가 시작위치.
         excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
@@ -20,8 +20,8 @@ public class AutoAppConfig {
 
     // 빈 이름 중복. 빈 이름 중복 시 자동 등록 빈 보다 수동 등록 빈이 우선적으로 등록됨.
     // 수동 빈이 자동 빈을 오버라이딩해버린다. -> 현재는 스프링 부트가 오류를 내도록 수정됨.
-//    @Bean(name = "memoryMemberRepository")
-//    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//    }
+/*    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }*/
 }
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -29,4 +29,17 @@ public class OrderServiceTest {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
+    /*
+    @Test
+    void fieldInjectionTest(){
+        OrderServiceImpl orderService = new OrderServiceImpl();
+//        orderService.createOrder(1L, "itemA", 10000);
+        // 자동 의존관계 주입: 필드 주입 방식 선택 시 NPE 터짐 -> setter 를 열어야 함 -> 수정자 주입 패턴으로 변경...
+        // 변경 가능성이 있음. 결국 요즘 잘 안씀...
+
+        orderService.setMemberRepository(new MemoryMemberRepository());
+        orderService.setDiscountPolicy(new FixDiscountPolicy());
+        orderService.createOrder(1L, "itemA", 10000);
+    }*/
 }
